ad89548dd5f5f8b47ef571bc86bacb6658393628
Brian Holloway <brian.holloway7025@gmail.com>
2024-06-19 16:20:25 -0500

    Installed react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom'
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import HomeCards from "./components/HomeCards";

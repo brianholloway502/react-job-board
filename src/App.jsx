62f70bff6b7e6843fc69c656b22b4e4da020b7cc
Brian Holloway <brian.holloway7025@gmail.com>
2024-06-20 15:57:03 -0500

    Added the ability to add jobs to the json server

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,34 @@ import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
 import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
-//import JobPage from './pages/JobPage';
 import JobPage, { jobLoader} from './pages/JobPage';
-
-// Create the React router. Directs the website pages. Components go on the page.
-const router = createBrowserRouter(
-  createRoutesFromElements(
-  <Route path='/' element={<MainLayout />}>
-    <Route index element={<HomePage />} />
-    <Route path='/jobs' element={<JobsPage />} />
-    <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
-    <Route path='/*' element={<NotFoundPage />} />
-  </Route>
-  )
-);
+import AddJobPage from './pages/AddJobPage';
 
 const App = () => {
+  // Add new job code. Function fired off after addJobSubmit is fired off in AddJobPage.
+  const addJob = async (newJob) => {
+    const res = await fetch('/api/jobs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newJob),
+    });
+    return
+  }
+  
+  // Create the React router. Directs the website pages. Components go on the page.
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path='/jobs' element={<JobsPage />} />
+      <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob} />} />
+      <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
+      <Route path='/*' element={<NotFoundPage />} />
+    </Route>
+    )
+  );
   return <RouterProvider router={router} />
 };
 

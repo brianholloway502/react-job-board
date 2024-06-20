c0095d3b86a675a75180a97ad8cd3f1506213153
Brian Holloway <brian.holloway7025@gmail.com>
2024-06-20 13:00:24 -0500

    JobPage using useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
 import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
+import JobPage from './pages/JobPage';
 
 // Create the React router. Directs the website pages. Components go on the page.
 const router = createBrowserRouter(
@@ -13,6 +14,7 @@ const router = createBrowserRouter(
   <Route path='/' element={<MainLayout />}>
     <Route index element={<HomePage />} />
     <Route path='/jobs' element={<JobsPage />} />
+    <Route path='/jobs/:id' element={<JobPage />} />
     <Route path='/*' element={<NotFoundPage />} />
   </Route>
   )

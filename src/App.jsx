f40f6f612def8d8ac60f0c32084c3471aa24dcc1
Brian Holloway <brian.holloway7025@gmail.com>
2024-06-20 13:30:22 -0500

    Made edits to job page to show job data from json db

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
 import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
-import JobPage from './pages/JobPage';
+//import JobPage from './pages/JobPage';
+import JobPage, { jobLoader} from './pages/JobPage';
 
 // Create the React router. Directs the website pages. Components go on the page.
 const router = createBrowserRouter(
@@ -14,7 +15,7 @@ const router = createBrowserRouter(
   <Route path='/' element={<MainLayout />}>
     <Route index element={<HomePage />} />
     <Route path='/jobs' element={<JobsPage />} />
-    <Route path='/jobs/:id' element={<JobPage />} />
+    <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
     <Route path='/*' element={<NotFoundPage />} />
   </Route>
   )
